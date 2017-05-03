19e4268d67d41f6e985b862dabc51ef3c4a6c92d
Natalman <sperobb@outlook.com>
2017-05-02 23:01:51 -0500

    Login

---
+++ PersoTrainer/Trainer/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+
+# Create your views here.
+from Trainer.forms import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.views.decorators.csrf import csrf_protect
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.shortcuts import render
+
+@csrf_protect
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+            email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+    variables = RequestContext(request)
+
+    return render(request,
+    'registration/register.html', {'form': form})
+
+@csrf_protect
+def register_success(request):
+    return render_to_response(
+    'registration/success.html',{},
+    RequestContext(request))
+
+@csrf_protect
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+@csrf_protect
+@login_required
+def home(request):
+    return render_to_response(
+    'home.html',
+    { 'user': request.user }
+    )
